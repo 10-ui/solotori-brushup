87dc9aec0cae69d952de94b5b3d1e64cc9945781
Sentaro <ui-10@outlook.jp>
2024-01-26 00:35:56 +0900

    Update width in fvText component

--- app/ui/home/fvText.tsx
+++ app/ui/home/fvText.tsx
@@ -23,7 +23,7 @@ export default function Copies() {
                    text-bases
                    justify-around 
                    absolute top-1/2 
-                   left-1/2 w-screen
+                   left-1/2 w-390
                    items-center
                    -translate-x-1/2
                    -translate-y-1/2
